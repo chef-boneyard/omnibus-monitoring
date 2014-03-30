260b929ebe3c9f1434bf95b1565331d9aab5e1dc
James Casey <james@opscode.com>
2014-03-30 13:30:40 -0700

    remove loading of private-chef config as it's not used

--- files/cookbooks/opscode-monitoring/recipes/default.rb
+++ files/cookbooks/opscode-monitoring/recipes/default.rb
@@ -26,12 +26,6 @@ directory "/etc/opscode-monitoring" do
   action :nothing
 end.run_action(:create)
 
-# We need to load the private chef configuration
-if File.exists?("/etc/opscode/chef-server-running.json")
-  private_chef = JSON.parse(IO.read("/etc/opscode/chef-server-running.json"))
-end
-node.consume_attributes({"private_chef" => private_chef['private_chef']})
-
 Monitoring[:node] = node
 if File.exists?("/etc/opscode-monitoring/opscode-monitoring.rb")
   Monitoring.from_file("/etc/opscode-monitoring/opscode-monitoring.rb")
